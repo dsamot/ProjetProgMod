8e896b09b9765cfa85f7d22198f354f7dc8c7723
Anas Raquy <anas.raquy@ensimag.grenoble-inp.fr>
2016-09-26 12:56:52 +0200

    src/MonteCarlo.cpp

--- pricer-skel/src/MonteCarlo.cpp
+++ pricer-skel/src/MonteCarlo.cpp
@@ -103,33 +103,7 @@ void MonteCarlo::delta(const PnlMat *past, double t, PnlVect *delta) {
         PnlMat *shift_path_down = pnl_mat_create(opt_->nbTimeSteps_ +1, mod_->size_); 
 
         PnlMat *path = pnl_mat_create(opt_->nbTimeSteps_ +1, mod_->size_);
-        //mod_->asset(path, t, maturite, opt_->nbTimeSteps_, rng_, past);
         double sommeDiffPayOff = 0;
-        //pnl_mat_print(path);
-
-        /*for (int idAsset=0; idAsset < mod_->size_; idAsset++) {
-            
-            for(int i=0; i < M; i++) {
-                //std::cout << "i: " << i << std::endl;
-                mod_->asset(path, t, maturite, opt_->nbTimeSteps_, rng_, past);
-                mod_->shiftAsset(shift_path_up, path, idAsset, fdStep_, t , (maturite / (double)opt_->nbTimeSteps_));
-                mod_->shiftAsset(shift_path_down, path, idAsset, -fdStep_, t , (maturite / (double)opt_->nbTimeSteps_));
-
-
-                sommeDiffPayOff += opt_->payoff(shift_path_up) - opt_->payoff(shift_path_down);
-            }
-            //std::cout << "sommeDiffPayOff: " << sommeDiffPayOff << std::endl;
-            facteurExp = expo/(M*2*pnl_mat_get(past, (past->m - 1), idAsset)*fdStep_);
-            //facteurExp = exp(-interet*(maturite - t))/(M*2*pnl_mat_get(past, (past->m - 1), idAsset)*fdStep_);
-            tempdelta = facteurExp*sommeDiffPayOff;
-            std::cout << "d: " << tempdelta << " indice: " << idAsset << std::endl;
-            pnl_vect_set(delta,idAsset, tempdelta);
-            sommeDiffPayOff = 0;
-        }*/
-
-        /*for (int idAsset=0; idAsset < mod_->size_; idAsset++) {
-            facteurExp = expo/(M*2*pnl_mat_get(past, (past->m - 1), idAsset)*fdStep_);
-        }*/
 
         for(int i=0; i < M; i++) {
             mod_->asset(path, t, maturite, opt_->nbTimeSteps_, rng_, past);
@@ -157,7 +131,7 @@ void MonteCarlo::delta(const PnlMat *past, double t, PnlVect *delta) {
 
 
 void MonteCarlo::delta(const PnlMat *past, double t, PnlVect *delta, PnlVect *ic) {
-    if (t < 0) {
+    /*if (t < 0) {
         std::cerr << "La date de pricing est négative!" << std::endl;
     } else if (t > opt_->T_) {
         std::cerr << "La date de pricing est supérieure à la maturité de l'option!" << std::endl;
@@ -194,6 +168,51 @@ void MonteCarlo::delta(const PnlMat *past, double t, PnlVect *delta, PnlVect *ic
             sommeDiffPayOff = 0;
             sommeDiffPayOffSquared = 0;
         }
+        pnl_mat_free(&shift_path_up);
+        pnl_mat_free(&shift_path_down);
+        pnl_mat_free(&path);
+    }*/
+    if (t < 0) {
+        std::cerr << "La date de pricing est négative!" << std::endl;
+    } else if (t > opt_->T_) {
+        std::cerr << "La date de pricing est supérieure à la maturité de l'option!" << std::endl;
+    } else {
+        double M = (double)nbSamples_;
+        double interet = mod_->r_ ;
+        double maturite = opt_->T_;
+        double tempdelta = 0;
+        double factor;
+        double expo = exp(-interet*(maturite - t));
+
+        PnlMat *shift_path_up = pnl_mat_create(opt_->nbTimeSteps_ +1, mod_->size_); 
+        PnlMat *shift_path_down = pnl_mat_create(opt_->nbTimeSteps_ +1, mod_->size_); 
+
+        PnlMat *path = pnl_mat_create(opt_->nbTimeSteps_ +1, mod_->size_);
+        double sommeDiffPayOff = 0;
+        PnlVect sommeDiffPayOffSquared = pnl_vect_create(mod_->size_);
+
+        for(int i=0; i < M; i++) {
+            mod_->asset(path, t, maturite, opt_->nbTimeSteps_, rng_, past);
+            for (int idAsset=0; idAsset < mod_->size_; idAsset++) {
+                mod_->shiftAsset(shift_path_up, path, idAsset, fdStep_, t , (maturite / (double)opt_->nbTimeSteps_));
+                mod_->shiftAsset(shift_path_down, path, idAsset, -fdStep_, t , (maturite / (double)opt_->nbTimeSteps_));
+                sommeDiffPayOff = opt_->payoff(shift_path_up) - opt_->payoff(shift_path_down);
+
+                pnl_vect_set(delta,idAsset,(pnl_vect_get(delta,idAsset) + sommeDiffPayOff));
+                pnl_vect_set(sommeDiffPayOffSquared,idAsset,(pnl_vect_get(sommeDiffPayOffSquared,idAsset) + sommeDiffPayOff*sommeDiffPayOff));
+            }
+        }
+
+        for (int idAsset=0; idAsset < mod_->size_; idAsset++) {
+            factor = 1/(2*pnl_mat_get(past, (past->m - 1), idAsset)*fdStep_);
+            double carreUn = pnl_vect_get(sommeDiffPayOffSquared,idAsset);
+            double carreDeux = pnl_vect_get(delta,idAsset);
+            pnl_vect_set(ic,idAsset,expo*factor*sqrt((carreUn/M - pow(carreDeux/M,2))/M)*1.96);
+            pnl_vect_set(delta,idAsset,(pnl_vect_get(delta,idAsset)*expo*factor/M));
+        }
+
+        pnl_vect_print(delta);
+        pnl_vect_print(ic);
         pnl_mat_free(&shift_path_up);
         pnl_mat_free(&shift_path_down);
         pnl_mat_free(&path);
